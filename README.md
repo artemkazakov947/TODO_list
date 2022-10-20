687b52abaa6e1e58a49e460a173626a04413f975
Artem Kazakov <artemkazakov947@gmail.com>
2022-10-20 08:48:02 +0300

    Todo_list final version

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 "# TODO_list" 
+![Logo of the project](https://www.drzimmerman.com/wp-content/uploads/2018/05/1973140.jpg)
+
+> Django project for managing task and task
+
+
+## Installing / Getting started
+
+You have to install Python 3!
+
+```shell
+git clone https://github.com/artemkazakov947/TODO_list.git
+cd TODO_list
+pip install virtualenv venv
+venv\Scripts\activate
+pip install -r requirementes.txt
+python manage.py runserver 
+```
+
+## Features
+
+
+* Managing task and tags directly on the website
+* You can set up deadline for your task as option
+* Strong Django admin panel for deep managing
--- app/views.py
+++ app/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from app.forms import TaskForm
+from app.models import Task, Tag
+
+
+class Home(generic.ListView):
+    model = Task
+    queryset = Task.objects.all()
+    template_name = "app/home.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("app:home")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("app:home")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("app:home")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    queryset = Tag.objects.all()
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("app:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("app:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("app:tag-list")
+
+
+def mark_complete_or_undo(request, pk):
+    task = Task.objects.get(id=pk)
+    if task.is_done is False:
+        task.is_done = True
+    else:
+        task.is_done = False
+    task.save()
+    return HttpResponseRedirect(reverse_lazy("app:home"))
